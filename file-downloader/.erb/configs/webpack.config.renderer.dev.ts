6affeaafe92c9561b1cac7b35a9f9c6a6235a05d
orchsik <orchsik@naver.com>
2022-12-27 11:46:58 +0900

    Update config

--- file-downloader/.erb/configs/webpack.config.renderer.dev.ts
+++ file-downloader/.erb/configs/webpack.config.renderer.dev.ts
@@ -173,7 +173,12 @@ const configuration: webpack.Configuration = {
     port,
     compress: true,
     hot: true,
-    headers: { 'Access-Control-Allow-Origin': '*' },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers':
+        'Origin, X-Requested-With, Content-Type, auth',
+      'Access-Control-Allow-Credentials': 'true',
+    },
     static: {
       publicPath: '/',
     },
