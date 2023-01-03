271815c4c7b253864e168529fe0b0d77ab0ece9e
orchsik <orchsik@naver.com>
2023-01-03 12:11:36 +0900

    Add: dark theme

--- file-downloader/src/renderer/App.tsx
+++ file-downloader/src/renderer/App.tsx
@@ -1,6 +1,8 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import './App.css';
 import './src/api/common';
@@ -9,18 +11,26 @@ import { StateProvider } from './src/data/StateProvider';
 import Hello from './src/pages/Hello';
 import ServiceSelector from './src/pages/ServiceSelector';
 import Downloader from './src/pages/Downloader';
-import SampleDown from './SampleDown';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export default function App() {
   return (
     <Router>
       <StateProvider>
-        <Routes>
-          {/* <Route path="/sampleDown" element={<SampleDown />} /> */}
-          <Route path="/" element={<Hello />} />
-          <Route path="/select" element={<ServiceSelector />} />
-          <Route path="/downloader" element={<Downloader />} />
-        </Routes>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <Routes>
+            {/* <Route path="/sampleDown" element={<SampleDown />} /> */}
+            <Route path="/" element={<Hello />} />
+            <Route path="/select" element={<ServiceSelector />} />
+            <Route path="/downloader" element={<Downloader />} />
+          </Routes>
+        </ThemeProvider>
       </StateProvider>
 
       <ToastContainer pauseOnFocusLoss={false} />
